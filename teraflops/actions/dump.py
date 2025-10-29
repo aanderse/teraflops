89dde61accf572df713e03d36f6698e281538dc0
Aaron Andersen <aanderse@tvbeat.com>
2025-10-29 13:28:08 +0000

    add 'dump' command

---
+++ teraflops/actions/dump.py
@@ -0,0 +1,11 @@
+
+from teraflops.utils import generate_full_terraform_config
+
+async def run(args):
+  async with generate_full_terraform_config():
+    with open('main.tf.json', 'r') as f:
+      print(f.read())
+
+def register_action(subparsers):
+  parser = subparsers.add_parser('dump', help='dump the generated terraform configuration as json')
+  parser.set_defaults(func=run)
