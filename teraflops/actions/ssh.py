f9f25b7f1dfcb00efa09e24207fe397a2882400d
Aaron Andersen <aanderse@tvbeat.com>
2025-12-02 15:42:19 +0000

    ssh: provide a helpful message if specified node is not found

--- teraflops/actions/ssh.py
+++ teraflops/actions/ssh.py
@@ -1,19 +1,28 @@
 
+import difflib
+import os
 import subprocess
+import tempfile
 
 from teraflops import nodes
 from teraflops import ssh
 
-import os
-import tempfile
-
 async def run(args):
   output_data = await nodes.get_teraflops_data()
+  available = output_data['nodes'].keys()
 
-  node = output_data['nodes'][args.node]
+  if args.node in available:
+    node = output_data['nodes'][args.node]
 
-  with ssh.get_private_key(output_data, node) as private_key:
-    subprocess.run(ssh.cmd(node, private_key=private_key))
+    with ssh.get_private_key(output_data, node) as private_key:
+      subprocess.run(ssh.cmd(node, private_key=private_key))
+  else:
+    matches = difflib.get_close_matches(args.node, available, n=5, cutoff=0.6)
+
+    if matches:
+        print(f'node `{args.node}` does not exist - did you mean one of: {', '.join(matches)}?')
+    else:
+        print(f'node `{args.node}` does not exist')
 
 def register_action(subparsers):
   parser = subparsers.add_parser('ssh', help='login on the specified machine via SSH')
