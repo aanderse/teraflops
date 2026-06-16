d8548d2ad8945e40d786554d20bbe7b25ed96757
Aaron Andersen <aaron@fosslib.net>
2026-06-16 09:11:43 -0400

    ssh: support -v, -vv, ... passthrough flags

--- teraflops/actions/ssh.py
+++ teraflops/actions/ssh.py
@@ -12,8 +12,10 @@ async def run(args):
     if args.node in available:
         node = output_data['nodes'][args.node]
 
+        extra_args = ['-' + 'v' * args.verbose] if args.verbose else None
+
         with ssh.get_private_key(output_data, node) as private_key:
-            subprocess.run(ssh.cmd(node, args.command or None, private_key=private_key))
+            subprocess.run(ssh.cmd(node, args.command or None, private_key=private_key, extra_args=extra_args))
     else:
         matches = difflib.get_close_matches(args.node, available, n=5, cutoff=0.6)
 
@@ -26,5 +28,6 @@ async def run(args):
 def register_action(subparsers):
     parser = subparsers.add_parser('ssh', help='login on the specified machine via SSH')
     parser.set_defaults(func=run)
+    parser.add_argument('-v', '--verbose', action='count', default=0, help='increase verbosity (passed through to ssh, e.g. -v, -vv, -vvv)')
     parser.add_argument('node', type=str, help='identifier of the node')
     parser.add_argument('command', nargs=argparse.REMAINDER, help='command to run')
